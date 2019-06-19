305c21ab297847922c43663184dd461a0db2f22a
WADA Hiroaki <hk1324@gmail.com>
2019-06-19 22:20:10 +0900

    add nutrition

---
+++ nutrition/precache-manifest.ebabc45214add5df4bf2eae0107801c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e32d6597f0a736594da6c2a495f840b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ba0daf6d6e46cacac93",
+    "url": "/nutrition/static/css/main.0b89a284.chunk.css"
+  },
+  {
+    "revision": "aaa5d5465b8a847c1a55",
+    "url": "/nutrition/static/js/2.741bda0b.chunk.js"
+  },
+  {
+    "revision": "6ba0daf6d6e46cacac93",
+    "url": "/nutrition/static/js/main.3602e1b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/nutrition/static/js/runtime~main.a8a9905a.js"
+  }
+]);
